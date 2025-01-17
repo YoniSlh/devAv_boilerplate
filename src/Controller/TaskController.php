366ee301c75751e5d27cf9aac985d503da778a88
Yoni Slh <yonisel12@gmail.com>
2025-01-17 15:39:48 +0100

    Exercice 1 OK

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TaskController extends AbstractController
+{
+    #[Route('/tasks/index', name: 'task_index')]
+    public function index(): Response
+    {
+        return $this->render('task/index.html.twig', [
+            'controller_name' => 'TaskController',
+        ]);
+    }
+
+    #[Route('/tasks/create', name: 'task_create')]
+    public function create(): Response
+    {
+        return $this->render('task/create.html.twig', [
+            'controller_name' => 'TaskController',
+        ]);
+    }
+
+    #[Route('/tasks/{id}/edit', name: 'task_edit')]
+    public function edit(int $id): Response
+    {
+        return $this->render('task/edit.html.twig', [
+            'controller_name' => 'TaskController',
+        ]);
+    }
+
+    #[Route('/tasks/{id}', name: 'task_view')]
+    public function view(int $id): Response
+    {
+        return $this->render('task/view.html.twig', [
+            'controller_name' => 'TaskController',
+        ]);
+    }
+
+    #[Route('/tasks/{id}/delete', name: 'task_delete')]
+    public function delete(int $id): Response
+    {
+        return new Response('Tâche supprimée : ' . $id);
+    }
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('admin@example.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'adminpass1'));
+
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('user@example.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'userpass1'));
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
